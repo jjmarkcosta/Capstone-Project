e92929171b4fdbc85eee934eb915d65e05378d89
jjmarkcosta <jjmarkcosta@gmail.com>
2024-08-02 21:08:54 -0400

    first commit

---
+++ src/main/java/com/nmscinema/Capstone_Project_New/CapstoneProjectNewApplication.java
@@ -0,0 +1,13 @@
+package com.nmscinema.Capstone_Project_New;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneProjectNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneProjectNewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nmscinema/Capstone_Project_New/CapstoneProjectNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nmscinema.Capstone_Project_New;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneProjectNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
